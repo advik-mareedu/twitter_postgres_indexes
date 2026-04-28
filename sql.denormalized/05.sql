267576ca6741c25ed25f284ace2fa525fd5860d8
Advik Mareedu <amareedu@g.hmc.edu>
2026-04-28 15:33:44 -0700

    only 05 left

---
+++ sql.denormalized/05.sql
@@ -0,0 +1,23 @@
+SELECT
+    '#' || tag AS tag, count(*) AS count
+FROM (
+    SELECT DISTINCT id,(jsonb->>'text'::TEXT) AS tag from(
+        SELECT 
+            data->> 'id' AS id, 
+            jsonb_array_elements(
+                COALESCE(data->'extended_tweet'->'entities'->'hashtags',data->'entities'->'hashtags','[]') ) AS jsonb
+        FROM tweets_jsonb
+        WHERE 
+        (data-> 'entities' -> 'hashtags' @> '[{"text": "coronavirus"}]'
+        OR 
+        data -> 'extended_tweet'-> 'entities' -> 'hashtags' @> '[{"text": "coronavirus"}]')
+        AND 
+        to_tsvector('english',COALESCE(data->'extended_tweet'->>'full_text',data->>'text'))@@to_tsquery('english','coronavirus')
+        AND 
+        (data ->> 'lang') = 'en'
+    ) AS sub ORDER BY id
+) AS t  
+GROUP BY tag
+ORDER BY count DESC, tag
+LIMIT 1000;
+
